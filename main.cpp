5db4f44cfdb176ab5e223b873e1342e1c97bfb5b
Dinda Aprilia <74056304+Dindaaprlia@users.noreply.github.com>
2021-01-29 10:43:39 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int angka;
+    cout<<"bilangan ganjil genap"<<endl;
+    cout<<"masukan batas maksimal angka;";
+    cin>>angka;
+    cout<<"bilangan genap"<<endl;
+    for(int i=1; i<=angka; i++)
+    {
+        if(i%2==0)
+        cout<<i<<"-";
+    }
+    cout<<" "<<endl;
+    cout<<"bilangan ganjil"<<endl;
+    for(int i=1; i<=angka; i++)
+    {
+        if (i%2!=0)
+            cout<<i<<" - ";
+    }
+}
